15547126468d4e0a19fb5d9f3a7dc9ef2dd89f60
Andreas Wennerberg <AndreasSE93@gmail.com>
2017-05-23 17:37:10 +0200

    Reads floats from stdin since argument line has a size limit

--- fp-cpu.c
+++ fp-cpu.c
@@ -3,19 +3,29 @@
 #include <time.h>      // clock_t, clock()
 
 // Compile and run with e.g.
-//     gcc fp-cpu.c && ./a.out {1..300000}
+//     gcc fp-cpu.c && (for i in {1..1000000}; do echo $i; done) | ./a.out
 
 int main(int argc, char * argv[]) {
-	unsigned int input_size = argc - 1;
-	float * input_floats = calloc(input_size, sizeof(float));
+	unsigned int array_size = 1;
+	unsigned int element_count = 0;
+	float * input_floats = malloc(sizeof(float[array_size]));
 	float val;
-	for (unsigned int i = 0; i < input_size; i++) {
-		int matches = sscanf(argv[i+1], "%f", &val);
-		if (matches != 1) {
-			fprintf(stderr, "Error reading argument %d.\n", i);
+	int matches;
+	while (matches != EOF) {
+		matches = scanf("%f", &val);
+		if (matches == 1) {
+			element_count++;
+			if (element_count > array_size) {
+				array_size *= 2;
+				input_floats = realloc(input_floats, sizeof(float[array_size]));
+			}
+			input_floats[element_count-1] = val;
+		} else if (EOF) {
+			break;
+		} else {  // Not a float or read error
+			fprintf(stderr, "Error reading input value %d.", element_count+1);
 			return EXIT_FAILURE;
 		}
-		input_floats[i] = val;
 	}
 
 	clock_t start, end;
@@ -24,7 +34,7 @@ int main(int argc, char * argv[]) {
 	start = clock();
 
 	float sum = input_floats[0];
-	for (int i = 1; i < input_size; i++) {
+	for (int i = 0; i < element_count; i++) {
 		sum += input_floats[i];
 	}
 
